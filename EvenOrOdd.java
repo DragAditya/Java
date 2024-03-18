024d6d9aa6c3fdcce443ee5f1b3fdba0a6df0786
DragAdi <93391458+DragAditya@users.noreply.github.com>
2024-03-18 20:58:33 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+package Simple4;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter The Number to Check Even or Odd: ");
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
