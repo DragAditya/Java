818a3ced5d4f6428c0975456baec9723f3632354
DragAdi <93391458+DragAditya@users.noreply.github.com>
2024-03-18 21:03:31 +0530

    Create Constructor.java

---
+++ Constructor.java
@@ -0,0 +1,11 @@
+class Const {
+    Const() {
+        System.out.println("Constructor Called");
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+        Const ref = new Const();
+    }
+}
